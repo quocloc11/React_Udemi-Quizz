3cef38deb6d070b945aa471ac0bde408a0c650c8
unknown <loc842211@gmail.com>
2024-04-19 11:02:52 +0700

    xu ly data

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react"
 import { useParams } from "react-router-dom"
 import { getDataQuiz } from "../../services/apiServices"
-
+import _ from 'lodash'
 const DetailQuiz = (props) => {
   const params = useParams()
   const quizId = params.id
@@ -12,6 +12,31 @@ const DetailQuiz = (props) => {
   const fetchQuestions = async () => {
     let res = await getDataQuiz(quizId)
     console.log('chwec', res)
+    if (res && res.EC === 0) {
+      let raw = res.DT
+      let data = _.chain(raw)
+        // Group the elements of Array based on `color` property
+        .groupBy("id")
+        // `key` is group's name (color), `value` is the array of objects
+        .map((value, key) => {
+          let answers = []
+          let questionDescription, image = null
+          value.forEach((item, index) => {
+            if (index === 0) {
+              questionDescription = item.questionDescription
+              image = item.image
+            }
+            answers.push(item.answers)
+            console.log('item', item.answers)
+          })
+          console.log("value", value, key)
+          return { questionId: key, answers, questionDescription, image }
+
+        }
+        )
+        .value()
+      console.log(data)
+    }
   }
   return (
     <div className="detail-quiz-container">
