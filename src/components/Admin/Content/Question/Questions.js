dd237c922638b3402620c450a2dc25b6d095b0bd
unknown <loc842211@gmail.com>
2024-04-21 16:21:48 +0700

    api add new question answers

--- src/components/Admin/Content/Question/Questions.js
+++ src/components/Admin/Content/Question/Questions.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Select from 'react-select';
 import './Questions.scss'
 import { AiOutlinePlusCircle } from "react-icons/ai";
@@ -8,15 +8,11 @@ import { AiOutlineMinusCircle } from "react-icons/ai";
 import { RiImageAddFill } from "react-icons/ri"
 import { v4 as uuidv4 } from 'uuid'
 import Lightbox from "react-awesome-lightbox";
+import { getAllQuizForAdmin, postCreateNewAnswerForQuestion, postCreateNewQuestionForQuiz } from "../../../../services/apiServices"
 
 import _ from 'lodash'
 const Questions = (props) => {
-  const options = [
-    { value: 'chocolate', label: 'Chocolate' },
-    { value: 'strawberry', label: 'Strawberry' },
-    { value: 'vanilla', label: 'Vanilla' },
-  ];
-  const [selectedQuiz, setSelectedQuiz] = useState({})
+
 
 
   const [questions, setQuestions] = useState(
@@ -43,6 +39,27 @@ const Questions = (props) => {
     title: '',
     url: ''
   })
+  const [listQuiz, setListQuiz] = useState([])
+  const [selectedQuiz, setSelectedQuiz] = useState({})
+
+  useEffect(() => {
+    fetQuiz()
+  }, [])
+
+  const fetQuiz = async () => {
+    let res = await getAllQuizForAdmin()
+    if (res && res.EC === 0) {
+      let newQuiz = res.DT.map(item => {
+        return {
+          value: item.id,
+          label: `${item.id} - ${item.description}`
+        }
+      })
+      setListQuiz(newQuiz)
+    }
+  }
+
+
   const handleAddRemoveQuestion = (type, id) => {
     if (type === 'ADD') {
       const newQuestion =
@@ -135,8 +152,23 @@ const Questions = (props) => {
     }
   }
 
-  const handleSubmitQuestionForQuiz = () => {
-    console.log('questions', questions)
+  const handleSubmitQuestionForQuiz = async () => {
+    //postCreateNewAnswerForQuestion,postCreateNewQuestionForQuiz
+
+    await Promise.all(questions.map(async (question) => {
+      const q = await postCreateNewQuestionForQuiz(
+        +selectedQuiz.value,
+        question.description,
+        question.imageFile)
+
+      await Promise.all(question.answers.map(async (answer) => {
+        await postCreateNewAnswerForQuestion(
+          answer.description, answer.isCorrect, q.DT.id
+
+        )
+      }))
+
+    }))
   }
   const handlePreviewImage = (questionId) => {
     let questionClone = _.cloneDeep(questions)
@@ -159,9 +191,9 @@ const Questions = (props) => {
         <div className='col-6 form-group'>
           <label className='mb-2'>Select Quiz:</label>
           <Select
-            defaultValue={selectedQuiz}
-            onChange={selectedQuiz}
-            options={options}
+            defaultValue={setSelectedQuiz}
+            onChange={setSelectedQuiz}
+            options={listQuiz}
           />
         </div>
         <div className='mt-3 mb-2'>
--- src/services/apiServices.js
+++ src/services/apiServices.js
@@ -69,9 +69,25 @@ const postCreateNewQuiz = (description, name, difficulty, image) => {
 const getAllQuizForAdmin = () => {
   return axios.get(`/api/v1/quiz/all`)
 }
+const postCreateNewQuestionForQuiz = (quiz_id, description, image) => {
+  const data = new FormData();
+  data.append('quiz_id', quiz_id);
+  data.append('description', description);
+  data.append('questionImage', image);
+  //su dung file dung form data ko truyen bth dc
+
+  return axios.post('api/v1/question', data)
+}
+const postCreateNewAnswerForQuestion = (description, correct_answer, question_id) => {
+  return axios.post('api/v1/answer', {
+    description, correct_answer, question_id
+  })
+}
 export {
   postCreateNewUser, getAllUsers, putUpdateUser,
   deleteUser, getUserWithPaginate, postLogin,
   postRegister, getQuizByUser, getDataQuiz, postSubmitQuiz,
-  postCreateNewQuiz, getAllQuizForAdmin
+  postCreateNewQuiz, getAllQuizForAdmin, postCreateNewQuestionForQuiz,
+  postCreateNewAnswerForQuestion
+
 }
